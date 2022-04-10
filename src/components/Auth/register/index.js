7ffdbd0be109e15cdc52dc8a3171dd74634fdf7a
EagleMind <hassen.ben.mbarek@gmail.com>
2022-04-11 00:22:29 +0100

    minor ui changes & cleanup

--- src/components/Auth/register/index.js
+++ src/components/Auth/register/index.js
@@ -7,21 +7,17 @@
  */
 
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   TextInput,
   Text,
   Button,
-  useColorScheme,
-  TouchableOpacity,
   View,
+  ImageBackground,
 } from 'react-native';
+
 import React, {useEffect, useState} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {register} from '../../../redux/actions/tmdb/auth';
-import {useSelector} from 'react-redux';
 import {DrawerActions} from '@react-navigation/native';
 export function Register({navigation}) {
   const [email, setEmail] = useState('test');
@@ -29,9 +25,7 @@ export function Register({navigation}) {
   useEffect(() => {
     navigation.dispatch(DrawerActions.closeDrawer());
   }, []);
-  function userHasAccount() {
-    AsyncStorage.clear().then(navigation.navigate('Login'));
-  }
+
   async function onSignUp(email, password) {
     register(email, password).then(res => {
       res ? navigation.navigate('Login') : alert('Oops, try again');
@@ -40,34 +34,48 @@ export function Register({navigation}) {
 
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={styles.loginTitle}>Register</Text>
-      </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Enter Email"
-          placeholderTextColor="white"
-          onChangeText={email => setEmail(email)}
-        />
-      </View>
+      <ImageBackground
+        source={require('../../../assets/bg.jpg')}
+        style={styles.background}>
+        <View style={styles.loginContainer}>
+          <View>
+            <Text style={styles.loginTitle}>Register</Text>
+          </View>
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Enter Email"
+              placeholderTextColor="white"
+              onChangeText={email => setEmail(email)}
+            />
+          </View>
 
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Enter password"
-          placeholderTextColor="white"
-          secureTextEntry={true}
-          onChangeText={password => setPassword(password)}
-        />
-      </View>
-      <Text>Already have an account?</Text>
-      <Button title="Login" onPress={() => userHasAccount()} />
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Enter password"
+              placeholderTextColor="white"
+              secureTextEntry={true}
+              onChangeText={password => setPassword(password)}
+            />
+          </View>
+          <View style={styles.loginBtn}>
+            <Text>Already have an account?</Text>
 
-      <Button
-        title="Register"
-        style={styles.loginText}
-        onPress={() => onSignUp(email, password)}></Button>
+            <Button
+              title="Login"
+              color={'black'}
+              onPress={() => navigation.navigate('Login')}
+            />
+          </View>
+          <View style={styles.mainButton}>
+            <Button
+              title="Register"
+              color={'black'}
+              onPress={() => onSignUp(email, password)}></Button>
+          </View>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -80,41 +88,53 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     color: 'white',
   },
-
+  background: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  loginContainer: {
+    flex: 0.7,
+    justifyContent: 'center',
+    margin: 50,
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: 10,
+  },
   image: {
     marginBottom: 40,
   },
 
   inputView: {
-    backgroundColor: '#3377FF',
+    backgroundColor: 'black',
     borderRadius: 30,
-    width: '70%',
+    width: '90%',
     height: 45,
-    marginBottom: 20,
+    marginVertical: 10,
     color: 'white',
     alignItems: 'center',
   },
 
   TextInput: {
-    height: 50,
-    flex: 1,
-    padding: 10,
-    marginLeft: 20,
     color: 'white',
   },
 
   loginBtn: {
-    width: '80%',
-    borderRadius: 25,
-    height: 50,
+    marginVertical: 10,
+    width: 200,
+    borderColor: 'black',
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 40,
-    backgroundColor: '#3377CF',
+    justifyContent: 'space-between',
+  },
+  mainButton: {
+    width: '80%',
   },
   loginTitle: {
-    color: 'grey',
-    fontSize: 20,
+    color: 'black',
+    fontSize: 25,
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
     marginBottom: 40,
   },
   loginText: {
